1ce820f54e1b0d4df7c6547b8ed665ccdd77c8f0
Saamahn Mahjouri <saams4u@bigmac.local>
2021-05-20 15:59:54 -0500

    first commit

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from AttentiveLayers import Fingerprint
+from AttentiveLayers_viz import Fingerprint_viz
+from getFeatures import save_smiles_dicts, get_smiles_dicts, get_smiles_array, moltosvg_highlight
